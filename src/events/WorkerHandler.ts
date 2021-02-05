05c9eb322ca17f72b461d2d65a07e1dae3d3e676
Dylan Hackworth <dylhack@outlook.com>
2021-02-05 09:03:36 -0600

    Added Unknown Task Handling

--- src/events/WorkerHandler.ts
+++ src/events/WorkerHandler.ts
@@ -17,9 +17,27 @@ export default class WorkerHandler {
   }
 
   public async onMessage(msg: ServerMessage): Promise<void> {
-    if (msg.task === 'get_member_roles') {
-      const [guildID, memberID] = msg.args;
-      await this.getRoles(msg.id, guildID, memberID);
+    const log = this.getLogger(msg.id);
+    log.debug(`Received task "${msg.task}"`);
+
+    try {
+      if (msg.task === 'get_member_roles') {
+        const [guildID, memberID] = msg.args;
+        const data = this.getRoles(msg.id, guildID, memberID);
+        const res: ServerResponse = {
+          id: msg.id,
+          data,
+        };
+        this.parent.postMessage(res);
+      } else {
+        throw new Error(`Unknown task "${msg.task}"`);
+      }
+    } catch (e) {
+      const res: ServerResponse = {
+        id: msg.id,
+        data: e,
+      };
+      this.parent.postMessage(res);
     }
   }
 
@@ -32,23 +50,18 @@ export default class WorkerHandler {
     id: string,
     guildID: string,
     userID: string,
-  ): Promise<void> {
-    const res: ServerResponse = {
-      id,
-      data: [],
-    };
-    try {
-      const guild = await this.modmail.guilds.fetch(guildID, true);
-      const member = await guild.members.fetch(userID);
+  ): Promise<string[]> {
+    const guild = await this.modmail.guilds.fetch(guildID, true);
+    const member = await guild.members.fetch(userID);
 
-      if (member === null) {
-        this.parent.postMessage(res);
-        return;
-      }
-      res.data = member.roles.cache.map((r) => r.id);
-      this.parent.postMessage(res);
-    } catch (_) {
-      this.parent.postMessage(res);
+    if (member === null) {
+      throw new Error("That member isn't in this guild.");
     }
+
+    return member.roles.cache.map((r) => r.id);
+  }
+
+  private getLogger(id: string) {
+    return Modmail.getLogger(`(worker-task) ${id}`);
   }
 }
