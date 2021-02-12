91f59622396ae3981aac6d182b06e614a5d75732
Dylan Hackworth <dylhack@outlook.com>
2021-02-12 06:59:26 -0600

    Updated send and receive colors

--- src/globals.ts
+++ src/globals.ts
@@ -11,7 +11,7 @@ export const IMAGE_REGEX = /\.|jpe?g|tiff?|png|webp|bmp$/i;
 export const MAX_THREADS = 30;
 
 export const COLORS = {
-  SEND: 0x7CFC00,
-  RECEIVE: 0xE8D90C,
+  SEND: 0x505DF4,
+  RECEIVE: 0xfed69a,
   INTERNAL: 0xADD8E6,
 };
