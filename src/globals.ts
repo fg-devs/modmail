d91893299b378363d30b87ff5b48212009fc1a7e
Dylan Hackworth <dylhack@outlook.com>
2021-02-12 14:56:02 -0600

    Added gif support

--- src/globals.ts
+++ src/globals.ts
@@ -6,7 +6,7 @@ export const PROMPT_TIME = 30000;
 
 export const CLOSE_THREAD_DELAY = 10000;
 
-export const IMAGE_REGEX = /\.|jpe?g|tiff?|png|webp|bmp$/i;
+export const IMAGE_REGEX = /\.|jpe?g|tiff?|png|gif|webp|bmp$/i;
 
 export const MAX_THREADS = 30;
 
