a37ddbddcba77abc819e7e3f15f90a05d2176878
Dylan Hackworth <dylhack@outlook.com>
2021-02-13 01:47:33 -0600

    Error handling for adding a duplicate sr

--- src/commands/standard_replies/create.ts
+++ src/commands/standard_replies/create.ts
@@ -3,6 +3,7 @@ import { Command, CommandoMessage } from 'discord.js-commando';
 import Modmail from '../../Modmail';
 import { CONFIG } from '../../globals';
 import { Requires } from '../../util/Perms';
+import LogUtil from '../../util/Logging';
 
 type Args = {
   name: string,
@@ -36,11 +37,24 @@ export default class StandardReplyCreate extends Command {
   @Requires(RoleLevel.Mod)
   public async run(msg: CommandoMessage, args: Args): Promise<null> {
     const pool = Modmail.getDB();
-    await pool.standardReplies.create(args.name, args.reply);
-    await msg.say(
-      'Successfully created a new standard reply'
-      + `\n - Usage: \`${CONFIG.bot.prefix}sr ${args.name}\``,
-    );
+
+    try {
+      await pool.standardReplies.create(args.name, args.reply);
+      await msg.say(
+        'Successfully created a new standard reply'
+        + `\n - Usage: \`${CONFIG.bot.prefix}sr ${args.name}\``,
+      );
+    } catch (err) {
+      let res;
+      if (err.message.includes('standard_replies_name_uindex')) {
+        res = 'That standard reply name already is taken.';
+      } else {
+        res = 'An internal issue occurred.';
+      }
+      LogUtil.cmdError(msg, err, res);
+      await msg.say(res);
+    }
+
     return null;
   }
 }
