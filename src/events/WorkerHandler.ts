4a7c69a0e7b58cebc52d6344b18bca0c05a4bd83
Dylan Hackworth <dylhack@outlook.com>
2021-02-14 15:24:56 -0600

    Added get Role and ChannelState

--- src/events/WorkerHandler.ts
+++ src/events/WorkerHandler.ts
@@ -1,11 +1,16 @@
-import { GuildMember, User } from 'discord.js';
 import {
+  Guild, GuildChannel,
+  GuildMember,
+  Role,
+  User,
+} from 'discord.js';
+import {
+  ChannelState,
   GetAllMemberStatesReq,
   GetMemberStateReq,
-  GetRolesReq,
-  GetUserStateReq,
+  GetRolesReq, GetStateReq,
   MemberState,
-  RoleLevel,
+  RoleLevel, RoleState,
   ServerMessage,
   ServerResponse,
   UserState,
@@ -31,7 +36,7 @@ export default class WorkerHandler {
 
   public async onMessage(msg: ServerMessage): Promise<void> {
     const log = WorkerHandler.getLogger(msg.id);
-    let res: ServerResponse = {
+    const res: ServerResponse = {
       data: null,
       id: msg.id,
     };
@@ -54,18 +59,26 @@ export default class WorkerHandler {
         const req = msg as GetAllMemberStatesReq;
         const [guildID, after, limit] = req.args;
         res.data = await this.getAllMemberStates(guildID, after, limit);
+        // Get a user
       } else if (msg.task === WORKER_CALLS.getUserState) {
-        const req = msg as GetUserStateReq;
+        const req = msg as GetStateReq;
         const [userID, cacheOnly] = req.args;
         res.data = await this.getUserState(userID, cacheOnly);
+        // Get a role
+      } else if (msg.task === WORKER_CALLS.getRole) {
+        const req = msg as GetStateReq;
+        const [roleID, cacheOnly] = req.args;
+        res.data = await this.getRoleState(roleID, cacheOnly);
+        // Get a channel
+      } else if (msg.task === WORKER_CALLS.getChannel) {
+        const req = msg as GetStateReq;
+        const [channelID, cacheOnly] = req.args;
+        res.data = await this.getChannelState(channelID, cacheOnly);
       } else {
         res.data = Error(`Unknown task "${msg.task}"`);
       }
     } catch (e) {
-      res = {
-        id: msg.id,
-        data: e,
-      };
+      res.data = e;
     }
     log.debug('Sent:\n', res);
     this.parent.postMessage(res);
@@ -81,10 +94,56 @@ export default class WorkerHandler {
     const member = await guild.members.fetch(userID);
 
     if (member === null) {
-      throw new Error("That member isn't in this guild.");
+      throw new Error('That member isn\'t in this guild.');
     }
 
     return member.roles.cache.map((r) => r.id);
+  }
+
+  public async getChannelState(
+    channelID: string,
+    cacheOnly: boolean,
+  ): Promise<ChannelState> {
+    if (!cacheOnly) {
+      const channel = await this.modmail.channels.fetch(channelID);
+      return WorkerHandler.parseChannel(channel as GuildChannel);
+    }
+
+    const channel = this.modmail.channels.cache.get(channelID);
+    if (channel === undefined) {
+      throw new Error('Channel does not exist in cache.');
+    }
+    return WorkerHandler.parseChannel(channel as GuildChannel);
+  }
+
+  public async getRoleState(
+    roleID: string,
+    cacheOnly: boolean,
+  ): Promise<RoleState> {
+    const guilds = this.modmail.guilds.cache.values();
+    const roleTasks: Promise<RoleState | null>[] = [];
+    let guildOpt = guilds.next();
+
+    while (!guildOpt.done) {
+      const guild = guildOpt.value;
+      const roleTask = WorkerHandler.getRole(roleID, guild, cacheOnly);
+      roleTasks.push(roleTask);
+      guildOpt = guilds.next();
+    }
+
+    const roles = await Promise.all(roleTasks);
+
+    for (let i = 0; i < roles.length; i += 1) {
+      const role = roles[i];
+      if (role !== null && role.id === roleID) {
+        return role;
+      }
+    }
+
+    if (cacheOnly) {
+      throw new Error('Role does not exist cache');
+    }
+    throw new Error('Role does not exist');
   }
 
   public async getUserState(
@@ -102,7 +161,7 @@ export default class WorkerHandler {
     user = await this.modmail.users.fetch(userID, true);
 
     if (user === null) {
-      throw new Error("That user doesn't in this guild.");
+      throw new Error('That user doesn\'t in this guild.');
     }
 
     return WorkerHandler.parseUser(user);
@@ -116,7 +175,7 @@ export default class WorkerHandler {
     const member = await guild.members.fetch(userID);
 
     if (member === null) {
-      throw new Error("That member isn't in this guild.");
+      throw new Error('That member isn\'t in this guild.');
     }
 
     const roleState = await WorkerHandler.getRoleState(member);
@@ -185,6 +244,26 @@ export default class WorkerHandler {
     };
   }
 
+  private static parseChannel(channel: GuildChannel): ChannelState {
+    return {
+      guildID: channel.guild.id,
+      id: channel.id,
+      isDeleted: channel.deleted,
+      parentID: channel.parentID,
+      type: channel.type,
+    };
+  }
+
+  private static parseRole(role: Role): RoleState {
+    return {
+      name: role.name,
+      color: role.color,
+      id: role.id,
+      position: role.position,
+      guildID: role.guild.id,
+    };
+  }
+
   private static async getRoleState(member: GuildMember): Promise<string> {
     const pool = Modmail.getDB();
     const dRoles = member.roles.cache.map((r) => r.id);
@@ -200,6 +279,27 @@ export default class WorkerHandler {
     }
 
     return roleState;
+  }
+
+  private static async getRole(
+    roleID: string,
+    guild: Guild,
+    cacheOnly: boolean,
+  ): Promise<RoleState | null> {
+    if (!cacheOnly) {
+      const role = await guild.roles.fetch(roleID);
+      if (role === null) {
+        return null;
+      }
+
+      return WorkerHandler.parseRole(role);
+    }
+
+    const role = guild.roles.cache.get(roleID);
+    if (role === undefined) {
+      return null;
+    }
+    return WorkerHandler.parseRole(role);
   }
 
   private static getLogger(id: string) {
