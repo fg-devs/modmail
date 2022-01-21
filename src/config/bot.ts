5819b5cf515cbd9a476eb8c4ddf0517b826bfa38
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2022-01-21 21:20:01 +1000

    leave guilds we shouldn't be in

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -7,10 +7,13 @@ export default class BotConfig extends Conf {
 
   public readonly owners: string[];
 
+  public readonly guildWhitelist: string[];
+
   constructor() {
     super('bot');
     this.token = '';
     this.prefix = '!';
     this.owners = ['Owner ID 1', 'Owner ID 2'];
+    this.guildWhitelist = [];
   }
 }
